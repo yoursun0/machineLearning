f25203d5950a215a16d242f37bb3a0d7f3c0decd
Helic Leung <yoursun0@gmail.com>
2017-02-16 00:35:59 +0800

    finish ex5
    complete ex5

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,19 +19,26 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
+J = sum((X*theta-y).^2)/2/m + lambda*sum(theta(2:end).*theta(2:end))/2/m; 
 
 % =========================================================================
 
-grad = grad(:);
+grad = (X'*((X*theta)-y))/m + lambda/m*[0; theta(2:end)];
 
 end
+
+%!test
+%! X = [[1 1 1]' magic(3)];
+%! y = [7 6 5]';
+%! theta = [0.1 0.2 0.3 0.4]';
+%! lambda = 0
+%! [J0 g0] = linearRegCostFunction(X, y, theta, lambda);
+%! [J7 g7] = linearRegCostFunction(X, y, theta, 7);
+%! Jexpected_lambda0 = 1.3533;
+%! Gexpected_lambda0 = [-1.4000; -8.7333; -4.3333; -7.9333;];
+%! Jexpected_lambda7 = 1.6917;
+%! Gexpected_lambda7 = [-1.4000; -8.2667; -3.6333; -7.0000;];
+%! assert(J0, Jexpected_lambda0, .0001);
+%! assert(g0, Gexpected_lambda0, .0001);
+%! assert(J7, Jexpected_lambda7, .0001);
+%! assert(g7, Gexpected_lambda7, .0001);
